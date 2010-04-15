d7d80f214995222aab2db9e5891d9fa0c39f86c7
Jingwei Wu <jingwei.wu@gmail.com>
2010-04-15 11:19:43 -0700

    Added two new constructors for DataCacheImpl

--- cds/src/impl/krati/cds/impl/DataCacheImpl.java
+++ cds/src/impl/krati/cds/impl/DataCacheImpl.java
@@ -10,6 +10,7 @@ import krati.cds.array.DataArray;
 import krati.cds.impl.array.CheckedDataArrayImpl;
 import krati.cds.impl.array.DataArrayImpl;
 import krati.cds.impl.array.fixed.LongArrayRecoverableImpl;
+import krati.cds.impl.segment.MemorySegmentFactory;
 import krati.cds.impl.segment.SegmentFactory;
 import krati.cds.impl.segment.SegmentManager;
 
@@ -23,6 +24,55 @@ public class DataCacheImpl implements DataCache
 {
     private final static Logger _log = Logger.getLogger(DataCacheImpl.class);
     private DataArray _dataArray;
+    
+    /**
+     * Constructs a data cache with default values below.
+     * <pre>
+     *    Segment File Size      : 512MB
+     *    Segment Compact Trigger: 0.2
+     *    Segment Compact Factor : 0.5
+     *    Redo entry size        : 10000
+     *    Number of Redo Entries : 5
+     *    Data integrity check   : No
+     *    Segment Factory        : MemorySegmentFactory
+     * </pre>
+     * 
+     * @param memberIdStart          Start of memberId
+     * @param memberIdCount          Total of memberId(s)
+     * @param cacheDirectory         Cache directory where persistent data will be stored
+     * @throws Exception
+     */
+    public DataCacheImpl(int memberIdStart,
+                         int memberIdCount,
+                         File cacheDirectory) throws Exception
+    {
+        this(memberIdStart, memberIdCount, cacheDirectory, new MemorySegmentFactory(), 512);
+    }
+    
+    /**
+     * Constructs a data cache with default values below.
+     * <pre>
+     *    Segment Compact Trigger: 0.2
+     *    Segment Compact Factor : 0.5
+     *    Redo entry size        : 10000
+     *    Number of Redo Entries : 5
+     *    Data integrity check   : No
+     *    Segment Factory        : MemorySegmentFactory
+     * </pre>
+     * 
+     * @param memberIdStart          Start of memberId
+     * @param memberIdCount          Total of memberId(s)
+     * @param cacheDirectory         Cache directory where persistent data will be stored
+     * @param segmentFileSizeMB      Segment size in MB
+     * @throws Exception
+     */
+    public DataCacheImpl(int memberIdStart,
+                         int memberIdCount,
+                         File cacheDirectory,
+                         int segmentFileSizeMB) throws Exception
+    {
+        this(memberIdStart, memberIdCount, cacheDirectory, new MemorySegmentFactory(), segmentFileSizeMB);
+    }
     
     /**
      * Constructs a data cache with default values below.
