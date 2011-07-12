dd7422e9ea80b8318527b31964c3644e928ec948
Jingwei Wu <jingwei.wu@gmail.com>
2011-07-11 18:48:09 -0700

    added interface Partition

---
+++ src/main/java/krati/util/Partition.java
@@ -0,0 +1,20 @@
+package krati.util;
+
+/**
+ * Partition
+ * 
+ * @author jwu
+ * 07/11, 2011
+ * 
+ * @param <K> Key
+ */
+public interface Partition<K> {
+    
+    /**
+     * Tests if this Partition contains a key.
+     * 
+     * @param key
+     * @return <code>true</code> if this Partition contains the key.
+     */
+    public boolean contains(K key);
+}
