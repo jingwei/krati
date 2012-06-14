34adddcdd98781648463cde1f60eb64ab460bf6e
jingwei <jingwei.wu@gmail.com>
2012-06-13 17:59:19 -0700

    updated javadoc

--- krati-main/src/main/java/krati/store/DataSetHandler.java
+++ krati-main/src/main/java/krati/store/DataSetHandler.java
@@ -26,17 +26,64 @@ import java.util.List;
  */
 public interface DataSetHandler extends DataHandler {
     
+    /**
+     * Counts the number of values in the specified <code>data</code>
+     *  
+     * @param data - the assembled data
+     * @return the number of values.
+     */
     public int count(byte[] data);
     
+    /**
+     * Assembles the specified value into a byte array.
+     * 
+     * @param value - the value
+     * @return the resulting byte array.
+     */
     public byte[] assemble(byte[] value);
     
+    /**
+     * Assembles the specified <code>value</code> with the specified <code>data</code>.
+     * 
+     * @param value - the value
+     * @param data  - the assembled data
+     * @return the resulting byte array
+     */
     public byte[] assemble(byte[] value, byte[] data);
     
+    /**
+     * Counts the number of collisions of the specified <code>value</code> in the <code>data</code>.
+     * 
+     * @param value - the value
+     * @param data  - the assembled data
+     * @return the number of values found in the specified <code>data</code> if the specified <code>value</code>
+     * is found. Otherwise, the negative number of values found in the <code>data</code>.
+     */
     public int countCollisions(byte[] value, byte[] data);
     
+    /**
+     * Removes the specified <code>value</code> from the assembled <code>data</code>.
+     * 
+     * @param value - the value
+     * @param data  - the assembled data
+     * @return the number of bytes left in the <code>data</code> after removing the <code>value</code>.
+     */
     public int remove(byte[] value, byte[] data);
     
+    /**
+     * Finds the specified <code>value</code> from the assembled <code>data</code>.
+     * 
+     * @param value - the value
+     * @param data  - the assembled data
+     * @return <code>true</code> if the <code>value</code> is found. Otherwise, <code>false</code>.
+     */
     public boolean find(byte[] value, byte[] data);
     
+    /**
+     * Extracts the values from the assembled <code>data</code>.
+     * 
+     * @param data the assembled data
+     * @return a list of values. 
+     */
     public List<byte[]> extractValues(byte[] data);
 }
--- krati-main/src/main/java/krati/store/DataStoreHandler.java
+++ krati-main/src/main/java/krati/store/DataStoreHandler.java
@@ -24,22 +24,80 @@ import java.util.Map.Entry;
  * 
  * @author jwu
  * 
+ * <p>
+ * 06/13, 2012 - Added javadoc <br/>
  */
 public interface DataStoreHandler extends DataHandler {
     
+    /**
+     * Assembles the specified <code>key</code> and <code>value</code> into a byte array.
+     * 
+     * @param key   - the key
+     * @param value - the value
+     * @return the resulting byte array
+     */
     public byte[] assemble(byte[] key, byte[] value);
     
+    /**
+     * Assembles the specified <code>key</code> and <code>value</code> into a byte array
+     * which is then combined with the specified <code>data</code> into the resulting byte array.
+     * 
+     * @param key   - the key
+     * @param value - the value
+     * @param data  - the assembled data
+     * @return the resulting byte array
+     */
     public byte[] assemble(byte[] key, byte[] value, byte[] data);
     
+    /**
+     * Counts the number of collisions of the specified <code>key</code> in the <code>data</code>.
+     * 
+     * @param key  - the key
+     * @param data - the assembled data
+     * @return the number of keys found in the specified <code>data</code> if the specified <code>key</code>
+     * is found. Otherwise, the negative number of keys found in the <code>data</code>.
+     */
     public int countCollisions(byte[] key, byte[] data);
     
+    /**
+     * Extracts the value mapped to the specified <code>key</code>.
+     * 
+     * @param key  - the key
+     * @param data - the assembled data
+     * @return the value
+     */
     public byte[] extractByKey(byte[] key, byte[] data);
     
+    /**
+     * Removes the specified <code>key</code> and its value from the specified <code>data</code>.
+     *  
+     * @param key  - the key
+     * @param data - the assembled data
+     * @return the number of bytes left in the <code>data</code> after removing the <code>key</code> and its value.
+     */
     public int removeByKey(byte[] key, byte[] data);
     
+    /**
+     * Extracts the keys from the specified <code>data</code>.
+     * 
+     * @param data - the assembled data
+     * @return a list of keys.
+     */
     public List<byte[]> extractKeys(byte[] data);
     
+    /**
+     * Extracts the mappings from the specified <code>data</code>.
+     * 
+     * @param data - the assembled data
+     * @return a list of Map.Entry values.
+     */
     public List<Entry<byte[], byte[]>> extractEntries(byte[] data);
     
+    /**
+     * Assembles a list of mappings into a byte array.
+     * 
+     * @param entries - the list of mappings
+     * @return the resulting byte array
+     */
     public byte[] assembleEntries(List<Entry<byte[], byte[]>> entries);
 }
