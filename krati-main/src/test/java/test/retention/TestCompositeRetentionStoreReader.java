1d239bfcc016bfb2de5aa4aeda7d107de92fce23
Ting Chen <tchen@linkedin.com>
2012-08-13 14:20:22 -0700

    Add a test to check if the client can correctly find it self fall out of retention

--- krati-main/src/test/java/test/retention/TestCompositeRetentionStoreReader.java
+++ krati-main/src/test/java/test/retention/TestCompositeRetentionStoreReader.java
@@ -1,5 +1,6 @@
 package test.retention;
 
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
 import junit.framework.TestCase;
 import krati.core.StoreConfig;
@@ -36,6 +37,7 @@ public class TestCompositeRetentionStoreReader<K,V> extends TestCase {
   
   protected SourceWaterMarksClock clock1; //clock for store1
   protected SourceWaterMarksClock clock2; //clock for store2
+  protected SourceWaterMarksClock clock3; //clock for store2
 
 
   protected File getHomeDir() {
@@ -207,29 +209,99 @@ public class TestCompositeRetentionStoreReader<K,V> extends TestCase {
     }
   }
 
-  public void testCompositeReaderWith3Substores() throws Exception
+  /**
+   * This test checks if the client can correctly find itself fall out of the retention
+   * @throws Exception
+   */
+
+  public void testCompositeRetentionStoreReaderFallOutOfRetention() throws Exception
   {
-    // Build a composite retention reader upon two retentions: one with two stores and another with one store
-    // Use three writer threads to update the underlying stores and their respected retentions
-    // Let a compositeRetentionReader to read from the retention from
-    //  (1) Clock.ZERO: then all updates should be read by the client.
+    // Build a composite retention reader upon three retentions each with one store
+    // Use three writer threads to update the underlying stores and the respected retentions
+    // Let a compositeRetentionReader to read from the retention 
 
     Retention<String> retention1 = createRetention(1);
     Retention<String> retention2 = createRetention(2);
-
+    Retention<String> retention3 = createRetention(3);
+    
     DataStore<String,String> store1 = createStore("s1");
     DataStore<String,String> store2 = createStore("s2");
     DataStore<String,String> store3 = createStore("s3");
 
-    clock1 = getClock("source1and2WaterMarks.scn", Lists.newArrayList(source1,source2));
-    clock2 = getClock("source3WaterMarks.scn", Lists.newArrayList(source3));
+    clock1 = getClock("source1WaterMarks.scn", Lists.newArrayList(source1));
+    clock2 = getClock("source2WaterMarks.scn", Lists.newArrayList(source2));
+    clock3 = getClock("source3WaterMarks.scn", Lists.newArrayList(source3));
 
 
     RetentionStoreWriter<String,String> writer1 =
             new SimpleRetentionStoreWriter<String,String>(source1,retention1,store1,clock1);
     RetentionStoreWriter<String,String> writer2 =
-            new SimpleRetentionStoreWriter<String,String>(source2,retention1,store2,clock1);
+            new SimpleRetentionStoreWriter<String,String>(source2,retention2,store2,clock2);
+    RetentionStoreWriter<String,String> writer3 =
+            new SimpleRetentionStoreWriter<String,String>(source3,retention3,store3,clock3);
 
+
+    SimpleRetentionStoreReader<String,String> singleReader1 =
+            new SimpleRetentionStoreReader<String,String>(source1,retention1,store1);
+
+    SimpleRetentionStoreReader<String,String> singleReader2 =
+            new SimpleRetentionStoreReader<String,String>(source2,retention2,store2);
+
+    SimpleRetentionStoreReader<String,String> singleReader3 =
+            new SimpleRetentionStoreReader<String,String>(source3,retention3,store3);
+
+    
+    // Start the writers to populate the stores
+    int cnt = getEventBatchSize() * getNumRetentionBatches();
+
+    long scn = System.currentTimeMillis();
+
+    HashSet<String> keySet = new HashSet<String>();
+    for(int i = 0; i < cnt; i++)
+    {
+      String key = nextKey();
+      String value1 = nextValue();
+      writer1.put(key,value1,scn++);
+      value1 = nextValue();
+      writer2.put(key,value1,scn++);
+      value1 = nextValue();
+      writer3.put(key,value1,scn++);
+      keySet.add(key);
+    }
+    System.out.println("Records inserted: " + cnt);
+
+    // Test the bootstrapping code
+    CompositeRetentionStoreReader compositeRetentionStoreReader = new CompositeRetentionStoreReader
+            (ImmutableList.of(singleReader1,singleReader2,singleReader3));
+
+    List<Event<String>> list = new ArrayList<Event<String>>();
+
+    Position pos = compositeRetentionStoreReader.getPosition(Clock.ZERO);
+
+    int resultCnt = 0;
+    do {
+      list.clear();
+      System.out.println("Position: " +  pos);
+      pos = compositeRetentionStoreReader.get(pos,list);
+      resultCnt += list.size();
+      System.out.println("Records read: " + resultCnt);
+    }while(list.size() > 0);
+
+    /*
+      Now the reader should catch up and we will test the streaming read
+    */
+    assertFalse(pos.isIndexed());
+    
+    // Insert another batch of records so that the client is out of retention again
+    for(int i = 0; i < getEventBatchSize() * getNumRetentionBatches();i++)
+    {
+      writer1.put(nextKey(),nextValue(),scn++);
+      writer2.put(nextKey(),nextValue(),scn++);
+    }
+
+    // Test if the client is out of retention
+    pos = compositeRetentionStoreReader.getPosition(pos.getClock());
+    assertNull(pos);
   }
 
   @Override
