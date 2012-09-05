e0fb58809539f9f80aed8249fd1284e6761d261b
Jingwei Wu <jwu@linkedin.com>
2012-09-05 14:23:46 -0700

    updated javadoc

--- krati-main/src/main/java/krati/core/segment/Segment.java
+++ krati-main/src/main/java/krati/core/segment/Segment.java
@@ -59,15 +59,39 @@ import java.nio.channels.WritableByteChannel;
  *
  */
 public interface Segment {
+    /**
+     * The segment storage version.
+     */
     public final static long STORAGE_VERSION = 1;
     
+    /**
+     * The default segment compaction factor (0.5).
+     */
     public final static double defaultSegmentCompactFactor = 0.5;
     
+    /**
+     * The default segment file size in MB (256).
+     */
     public final static int defaultSegmentFileSizeMB = 256;
+    
+    /**
+     * The maximum segment file size in MB (2048).
+     */
     public final static int maxSegmentFileSizeMB = 2048;
+    
+    /**
+     * The minimum segment file size in MB (8).
+     */
     public final static int minSegmentFileSizeMB = 8;
     
+    /**
+     * The position (i.e. offset) for last forced time within segment.
+     */
     public final static int posLastForcedTime = 0;
+    
+    /**
+     * The position (i.e. offset) for storage version within segment.
+     */
     public final static int posStorageVersion = 8;
     
     /**
@@ -75,58 +99,208 @@ public interface Segment {
      */
     public final static int dataStartPosition = 128;
     
+    /**
+     * @return the segment {@link Segment.Mode Mode}.
+     */
     public Mode getMode();
     
+    /**
+     * @return the segment Id.
+     */
     public int getSegmentId();
     
+    /**
+     * @return the segment file.
+     */
     public File getSegmentFile();
     
+    /**
+     * @return the initial segment size measured in bytes.
+     */
     public long getInitialSize();
     
+    /**
+     * @return the initial segment size measured in MB.
+     */
     public int getInitialSizeMB();
     
+    /**
+     * @return the segment load measured in bytes.
+     */
     public int getLoadSize();
     
+    /**
+     * @return the segment load factor (i.e., the percentage of segment in use).
+     */
     public double getLoadFactor();
     
+    /**
+     * Increases the segment load size.
+     * 
+     * @param byteCnt - the number of bytes to add.
+     */
     public void incrLoadSize(int byteCnt);
     
+    /**
+     * Decreases the segment load size.
+     * 
+     * @param byteCnt - the number of bytes to subtract.
+     */
     public void decrLoadSize(int byteCnt);
     
+    /**
+     * Gets the segment append position.
+     * 
+     * @return the current append position of this segment.
+     * @throws IOException
+     */
     public long getAppendPosition() throws IOException;
     
+    /**
+     * Sets the segment append position.
+     * 
+     * @param pos - the new append position
+     * @throws IOException
+     */
     public void setAppendPosition(long pos) throws IOException;
     
+    /**
+     * Reads an integer value from the specified position.
+     * 
+     * @param pos - the position (i.e. offset) in segment
+     * @return an integer value
+     * @throws IOException
+     */
     public int readInt(int pos) throws IOException;
     
+    /**
+     * Reads a long value from the specified position.
+     * 
+     * @param pos - the position (i.e. offset) in segment
+     * @return a long value
+     * @throws IOException
+     */
     public long readLong(int pos) throws IOException;
     
+    /**
+     * Reads a short value from the specified position.
+     * 
+     * @param pos - the position (i.e. offset) in segment
+     * @return a short value
+     * @throws IOException
+     */
     public short readShort(int pos) throws IOException;
     
+    /**
+     * Reads bytes from the specified position into a byte array.
+     * 
+     * @param pos - the segment position
+     * @param dst - the destination byte array
+     * @throws IOException
+     */
     public void read(int pos, byte[] dst) throws IOException;
     
+    /**
+     * Reads bytes from the specified position into a byte array.
+     * 
+     * @param pos    - the segment position
+     * @param dst    - the destination byte array
+     * @param offset - the offset to the byte array
+     * @param length - the number of bytes to read from the specified position
+     * @throws IOException
+     */
     public void read(int pos, byte[] dst, int offset, int length) throws IOException;
     
+    /**
+     * Appends an integer value.
+     * 
+     * @return the segment position at which the specified value is appended.
+     * @throws IOException
+     */
     public int appendInt(int value) throws IOException;
     
+    /**
+     * Appends a long value.
+     * 
+     * @return the segment position at which the specified value is appended.
+     * @throws IOException
+     */
     public int appendLong(long value) throws IOException;
     
+    /**
+     * Appends a short value.
+     * 
+     * @return the segment position at which the specified value is appended.
+     * @throws IOException
+     */
     public int appendShort(short value) throws IOException;
     
+    /**
+     * Appends the specified byte array.
+     * 
+     * @return the segment position at which the specified data is appended.
+     * @throws IOException
+     */
     public int append(byte[] data) throws IOException;
     
+    /**
+     * Appends bytes from the specified byte array.
+     * 
+     * @param data   - the byte array
+     * @param offset - the offset to the byte array
+     * @param length - the number of bytes from the byte array to append
+     * @return the segment position at which the specified data is appended.
+     * @throws IOException
+     */
     public int append(byte[] data, int offset, int length) throws IOException;
     
+    /**
+     * Transfers bytes to a target segment.
+     * 
+     * @param pos           - the segment position
+     * @param length        - the number of bytes to read from the specified position
+     * @param targetSegment - the target segment to append to
+     * @return the number of bytes transferred
+     * @throws IOException
+     */
     public int transferTo(int pos, int length, Segment targetSegment) throws IOException;
     
+    /**
+     * Transfers bytes to a target writable channel.
+     * 
+     * @param pos           - the segment position
+     * @param length        - the number of bytes to read from the specified position
+     * @param targetChannel - the target writable channel to append to
+     * @return the number of bytes transferred
+     * @throws IOException
+     */
     public int transferTo(int pos, int length, WritableByteChannel targetChannel) throws IOException;
     
+    /**
+     * Tests if this segment is read-only.
+     */
     public boolean isReadOnly();
     
+    /**
+     * Changes this segment to read-only.
+     * 
+     * @throws IOException
+     */
     public void asReadOnly() throws IOException;
     
+    /**
+     * Forces updates to this segment to disk.
+     * 
+     * @throws IOException
+     */
     public void force() throws IOException;
     
+    /**
+     * Closes this segment to disable read/write operations.
+     * 
+     * @param force - whether to force updates to this segment to disk 
+     * @throws IOException
+     */
     public void close(boolean force) throws IOException;
     
     /**
@@ -164,6 +338,9 @@ public interface Segment {
      */
     public String getStatus();
     
+    /**
+     * The segment mode.
+     */
     public static enum Mode {
         READ_ONLY,
         READ_WRITE;
